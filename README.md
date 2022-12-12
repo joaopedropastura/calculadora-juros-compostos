a479741d288822e9009cc1d708858994d1ebcd8b
jpsoares01 <jpsoares111@hotmail.com>
2022-12-12 11:51:46 -0300

    eu sou burro?

---
+++ README.md
@@ -0,0 +1,20 @@
+funcionalidade para implementção:
+
+- input aporte incial
+- input aportes mensais
+- input taxa de juros
+- input data incial
+- input vencimento 
+- select entre taxa anual ou taxa mensal
+
+
+link referencia: https://www.mobills.com.br/calculadoras/calculadora-juros-compostos/
+
+https://www.tabnine.com/academy/javascript/get-value-of-input/
+
+
+gradient css:
+
+    https://cssgradient.io/
+
+    https://uigradients.com/#Nighthawk
--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-var initValue = document.getElementById("calculadora");
-console.log(initValue)
+
+function getVal() {
+    const initValue = document.getElementById("valorInit").value
+    const monthValue = document.getElementById("valorInter").value
+    const taxValue = document.getElementById("tax").value
+    const month = document.getElementById("month").value
+    calculaMontante(initValue, monthValue, month, taxValue)
+}
+
+function calculaMontante(init, monthValue, month, tax) {
+    console.log(init, monthValue, month, tax)
+    let montanteFinal = init
+    tax = tax/100
+    for (i = 0; i < month; i++)
+    {   
+        montanteFinal += monthValue *Math.pow((1+tax), )
+    }
+    console.log(montanteFinal)
+}
+
+
