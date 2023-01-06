387e2f798ccbe2683f3476c880e45ecc339459f0
jpsoares01 <jpsoares111@hotmail.com>
2023-01-06 16:42:08 -0300

    onfire

--- script.js
+++ script.js
@@ -4,17 +4,24 @@ function getVal() {
     const monthValue = parseInt($("#valorInter").val())
     const taxValue =  parseInt($("#tax").val())
     const month = parseInt($("#month").val())
-    calculaMontante(initValue, monthValue, month, taxValue)
+    const opt_taxa = $("#opt-taxa").val()
+    calculaMontante(initValue, monthValue, month, taxValue, opt_taxa)
 }
 
-function calculaMontante(init, monthValue, month, tax) {
-    console.log(init, monthValue, month, tax)
+function calculaMontante(init, monthValue, month, tax, opt_taxa) {
+    
+    console.log(opt_taxa)
+    var opt_val = opt_taxa == "Anual" ? ((Math.pow((1+(tax/100)),(1/12)))-1) : tax
+    console.log("opt_taxa =" + opt_taxa)
+    tax = opt_val;
 
-    tax = ((Math.pow((1+(tax/100)),(1/12)))-1)*100
-    tax = tax/100
-    let fv = init * (Math.pow(1+tax, month)) + (monthValue *(Math.pow(1+tax, month) - 1)/ tax)
-
-    console.log(fv)
+    console.log("taxa =" + tax)
+    let montanteFinal = init * (Math.pow(1+tax, month)) + (monthValue *(Math.pow(1+tax, month) - 1)/ tax)
+    let valorInvestido = init + (monthValue*month)
+    let taxAcumulado = montanteFinal - valorInvestido
+    console.log(montanteFinal)
+    console.log(valorInvestido)
+    console.log(taxAcumulado)
 }
 
 
