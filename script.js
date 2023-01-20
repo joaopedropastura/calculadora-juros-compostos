2d3af9ed1407ddce528e1deee418ed1ea77e2abf
jpsoares01 <jpsoares111@hotmail.com>
2023-01-20 16:42:31 -0300

    funcionando no html

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ function getVal() {
     const taxValue =  parseInt($("#tax").val())
     const month = parseInt($("#month").val())
     const opt_taxa = $("#opt-taxa").val()
+
     calculaMontante(initValue, monthValue, month, taxValue, opt_taxa)
 }
 
 function calculaMontante(init, monthValue, month, tax, opt_taxa) {
 
     console.log(opt_taxa)
+    const $result = $("#resultado")
     var opt_val = opt_taxa == "Anual" ? ((Math.pow((1+(tax/100)),(1/12)))-1) : tax/100
     console.log("opt_taxa =" + opt_taxa)
     tax = opt_val;
@@ -21,9 +23,11 @@ function calculaMontante(init, monthValue, month, tax, opt_taxa) {
     let montanteFinal = init * (Math.pow(1+tax, month)) + (monthValue *(Math.pow(1+tax, month) - 1)/ tax)
     let valorInvestido = init + (monthValue*month)
     let taxAcumulado = montanteFinal - valorInvestido
-    console.log(montanteFinal)
     console.log(valorInvestido)
     console.log(taxAcumulado)
+    console.log(montanteFinal)
+
+    $result.text($result.text() + montanteFinal)
 }
 
 //taxa de juros selic
